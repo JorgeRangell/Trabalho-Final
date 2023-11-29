fe23c52f550c1101efc35310e5735dd97f663062
Jorge Rangel <101427212+JorgeRangell@users.noreply.github.com>
2023-11-29 11:58:50 -0300

    Delete Trabalho final faculdade directory

--- Trabalho final faculdade/check_login.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'bd_connect.php';
-    session_start();
-    
-    if(!isset($_SESSION['usuario'])){
-            header('location:index.php?erro_login=true');
-            exit;
-    }
-?>
